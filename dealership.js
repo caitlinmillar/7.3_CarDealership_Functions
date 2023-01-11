98c1c3028d00d6ce8cd4c40811b6ae5a3232844d
Caitlin Millar <caitlin-millar@outlook.com>
2023-01-11 15:40:00 +0000

    setting up dealership class

---
+++ dealership.js
@@ -0,0 +1,10 @@
+const Car = require('./car')
+const Dealership = function(name, maxNoOfCars, stock){
+    this.name = name;
+    this.maxNoOfCars = maxNoOfCars;
+    this.stock = stock;
+};
+
+Car.prototype.countStock = function(){
+    return 
+}

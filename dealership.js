9226bf55be0e73496fc9b4c9821a27256d1f3cf9
Caitlin Millar <caitlin-millar@outlook.com>
2023-01-11 16:10:01 +0000

    added car objects and count stock function

--- dealership.js
+++ dealership.js
@@ -5,6 +5,26 @@ const Dealership = function(name, maxNoOfCars, stock){
     this.stock = stock;
 };
 
-Car.prototype.countStock = function(){
-    return 
-}
+const myDealership = new Dealership("My dealership", 300, 175);
+
+cars = [
+    {manufacturer: "FORD", price: 2780, engine: 2.0},
+    {manufacturer: "TOYOTA", price: 2780, engine: 2.0},
+    {manufacturer: "SEAT", price: 2780, engine: 2.0},
+    {manufacturer: "LAND ROVER", price: 2780, engine: 2.0},
+    {manufacturer: "AUDI", price: 2780, engine: 2.0},
+    {manufacturer: "CITREON", price: 2780, engine: 2.0},
+    {manufacturer: "HONDA", price: 2780, engine: 2.0},
+    {manufacturer: "VOLVO", price: 2780, engine: 2.0},
+    {manufacturer: "MINI", price: 2780, engine: 2.0},
+    {manufacturer: "NISSAN", price: 2780, engine: 2.0},
+    {manufacturer: "FIAT", price: 2780, engine: 2.0},
+    {manufacturer: "MAZDA", price: 2780, engine: 2.0},
+    {manufacturer: "SUZUKI", price: 2780, engine: 2.0},
+    {manufacturer: "SKODA", price: 2780, engine: 2.0},
+]
+
+const countStock = (stock) => {
+    return stock.length();
+};
+

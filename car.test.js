a167baa41518106f53516887f13cb9da55fa0657
Caitlin Millar <caitlin-millar@outlook.com>
2023-01-14 12:19:47 +0000

    fixed error: error was I declared an instantiation of a car object in beforeEach however when asking my tests to call upon this object it wouldn't work, I had to make a new car at the start of every test. Solution: I had declared the new object as newCar but got confused and thought the test needed to just call a car object, no I had named it newCar to clarify it was a new object. Edited to match my original naming.

--- car.test.js
+++ car.test.js
@@ -1,29 +1,31 @@
 const Car = require('./car');
+// {Car} destructures the class, gets a car thing from in car, we don't have one
+// if there were three 'classes' car, train, plan we import using {}
 
 let newCar;
 
 beforeEach(()=>{
-    newCar = new Car("VroomVroom", 20, 5.2);
+    newCar = new Car("VroomVroom", 5000, 1.4);
 })
 
 describe('testing car class', ()=>{
     test('can get manufacturer', ()=>{
-        const car = new Car("BMW", 5000, 1.4);
-        actual = car.getManufacturer();
-        expected = "BMW";
+        //const car = new Car("BMW", 5000, 1.4);
+        const actual = newCar.getManufacturer();
+        const expected = "VroomVroom";
         expect(actual).toBe(expected);
     });
     test('can get price', ()=>{
-        const car = new Car("BMW", 5000, 1.4);
-        expect(car.getPrice()).toBe(5000);
+        // const car = new Car("BMW", 5000, 1.4);
+        expect(newCar.getPrice()).toBe(5000);
     });
     test('can get engine type', ()=>{
-        const car = new Car("BMW", 5000, 1.4);
-        expect(car.getEngine()).toBe(1.4);
+        //const car = new Car("BMW", 5000, 1.4);
+        expect(newCar.getEngine()).toBe(1.4);
     });
     it('should be able to make new Car objects', ()=>{
-        const car = new Car();
-        expect(car).toBeTruthy();
+        //const car = new Car();
+        expect(newCar).toBeTruthy();
     });    
 });
 
